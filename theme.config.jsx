b08080e6db5e75ca41b41f134bb70a411c87c6f5
Bruno de Araújo Alves <me@baraus.dev>
2023-05-10 16:56:59 -0300

    Update

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+import NextImage from 'next/image';
+
+export default {
+  logo: <NextImage src="/img/badomen_horizontal.png" width={180} height={48} />,
+  project: {
+    link: 'https://github.com/shuding/nextra',
+  },
+  // ...
+};
